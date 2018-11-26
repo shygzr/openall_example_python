411009f4d9638a811c16fe8ce28f2f9f17909c66
MyPC <shygzr@gmail.com>
2018-11-26 21:13:19 +0800

    Initial commit

---
+++ main.py
@@ -0,0 +1,22 @@
+#!/usr/bin/evn python
+# coding=utf-8
+
+from bottle import default_app, get, run
+from beaker.middleware import SessionMiddleware
+
+# 设置session参数
+session_opts = {
+    'session.type': 'file',
+    'session.cookie_expires': 3600,
+    'session.data_dir': '/tmp/sessions/simple',
+    'session.auto': True
+}
+
+@get('/')
+def callback():
+    return '济南大学智慧校园开放平台_python应用_bottle框架'
+
+# 函数主入口
+if __name__ == '__main__':
+    app_argv = SessionMiddleware(default_app(), session_opts)
+    run(app=app_argv, host='0.0.0.0', port=9090, debug=True, reloader=True)
